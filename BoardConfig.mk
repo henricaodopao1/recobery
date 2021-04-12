623d6230c70e743011528bd43f166d2f8334a6a3
Vivekachooz <vivekachooz@gmail.com>
2021-04-12 09:51:07 +0530

    X00TD: Switch to file based encryption
    * Also cleanup fstab
    
    Change-Id: I81c42a48bcd9eec0296428aa6832fcb294b7917d

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -32,6 +32,7 @@ TARGET_BOOTLOADER_BOARD_NAME := sdm660
 TARGET_NO_BOOTLOADER := true
 
 # Crypto
+TW_INCLUDE_CRYPTO := true
 TARGET_CRYPTFS_HW_PATH := vendor/qcom/opensource/commonsys/cryptfs_hw
 TARGET_HW_DISK_ENCRYPTION := true
 
